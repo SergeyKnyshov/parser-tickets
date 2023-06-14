7e99d67abb1c050478e7a587e5c5479db406c23e
Soerega <soerega74@gmail.com>
2023-06-14 12:34:35 +0500

    Delete duration

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-from transliterate import translit
-
-print(translit('Омск', 'ru', reversed=True))
+import datetime
+date1 = datetime.datetime.today()
+date2 = datetime.timedelta(hours=1)
+date1 += date2
+date1 = date1 - datetime.datetime.today()
+print(datetime.datetime(0, 0, 0))
