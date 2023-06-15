d99c8da8b4095255866b5122de870987d257b1b3
Soerega <soerega74@gmail.com>
2023-06-16 02:51:25 +0500

    update parser

--- parser/pars_controller.py
+++ parser/pars_controller.py
@@ -18,12 +18,12 @@ class ParserController:
         if not self.db.check_exist_trip_by_route(self.origin, self.destination):
             tickets_lst = []
             try:
-                crawl_web1 = self.__get_avia_tickets_from_web()
+                crawl_web1 = self.__get_tickets_from_web(ParserAviaSales)
                 tickets_lst.extend(crawl_web1)
             except:
                 print(f'Отсутствует маршрут {self.origin}-{self.destination} на aviasales')
             try:
-                crawl_web2 = self.__get_rail_tickets_from_web()
+                crawl_web2 = self.__get_tickets_from_web(ParserRailway)
                 tickets_lst.extend(crawl_web2)
             except:
                 print(f'Отсутствует маршрут {self.origin}-{self.destination} на РЖД')
@@ -38,19 +38,15 @@ class ParserController:
             return False
         
         
-    def __get_avia_tickets_from_web(self):
-        crawl = CrawlerAviaSales(self.origin, self.destination, self.date)
+    def __get_tickets_from_web(self, parser):
+        crawl = parser(self.origin, self.destination, self.date)
         crawl_web = crawl.get_html()
         
-        pars = ParserAviaSales(crawl_web)
-        return pars.get_tickets()
+        pars = parser(crawl_web)
+        res = pars.get_tickets()
+        res['duration'] = res['destination_date'] - res['origin_date']
+        return res
     
-    def __get_rail_tickets_from_web(self):
-        crawl = CrawlerRailway(self.origin, self.destination, self.date)
-        crawl_web = crawl.get_html()
-        
-        pars = ParserRailway(crawl_web)
-        return pars.get_tickets()
     
     def add_tickets_into_db(self, lst_of_tickets):
         
